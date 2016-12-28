d3d17789f58016898c4c74808e14612634e9535a
王树权 <3346148964@qq.com>
2016-12-28 09:58:58 +0800

    Initial Commit

---
+++ WSQNetWorkingSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSQNetWorkingSystem
+//
+//  Created by webapps on 16/12/28.
+//  Copyright © 2016年 webapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSQNetWorkingSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSQNetWorkingSystem
+//
+//  Created by webapps on 16/12/28.
+//  Copyright © 2016年 webapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
